a9cd40b559432e08f089e571c8d891ceb30975a3
Inna Denysiuk <innadenysiuk275@gmail.com>
2025-10-12 14:23:44 +0300

    Сreate Flask app with root and healthcheck endpoints

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import jsonify
+from datetime import datetime
+
+
+@app.route("/")
+def greeting():
+    return jsonify({
+        "message": "Hello!"
+    }), 200
+
+
+@app.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    return jsonify({
+        "status": "ok", 
+        "timestamp": datetime.utcnow().isoformat() + "Z"
+    }), 200
